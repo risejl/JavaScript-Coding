1fc65022a106c1e105b52d2bc3ab5a6d5abb41bd
Mitchell Cheng <chengbotier1@gmail.com>
2024-11-01 15:50:46 +0800

    fix: rewrite closure challenges

---
+++ Closure/Sum/sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ */
+
+function sum(num) {
+  const func = function (num2) {
+    return num2 ? sum(num + num2) : num;
+  };
+
+  func.valueOf = () => num;
+  return func;
+}
+
+// Usage example
+const sum1 = sum(1);
+console.log(sum1(2) == 3); // => true
+console.log(sum1(3) == 4); // => true
+console.log(sum(1)(2)(3) == 6); // => true
+console.log(sum(5)(-1)(2) == 6); // => true
